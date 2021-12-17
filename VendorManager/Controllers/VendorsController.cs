89ffacbfa3594e5c9e9f1071fc443e70ffcdc2b2
Hannah Young <hannah@corgibyte.com>
2021-12-17 13:58:54 -0800

    Implement Home and Vendors controllers

---
+++ VendorManager/Controllers/VendorsController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorManager.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace VendorManager.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      return View(Vendor.GetAll());
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors/")]
+    public ActionResult Create(IFormCollection formContents)
+    {
+      string name = formContents["name"];
+      string contactName = formContents["contactName"];
+      string contactEmail = formContents["contactEmail"];
+      string contactPhone = formContents.ContainsKey("contactPhone") ? formContents["contactPhone"] : "Not given";
+      string contactPronouns = formContents.ContainsKey("contactPronouns") ? formContents["contactPronouns"] : "Not given";
+      Contact newContact = new(contactName, contactEmail, contactPhone, contactPronouns);
+      Vendor newVendor = new(name, newContact);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{vendorId}")]
+    public ActionResult Show(int vendorId)
+    {
+      List<Order> vendorsOrders = Vendor.Find(vendorId).Orders;
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("orders", vendorsOrders);
+      model.Add("vendor", Vendor.Find(vendorId));
+      return View(model);
+    }
+
+    [HttpGet("/vendors/{vendorId}/edit")]
+    public ActionResult Edit(int vendorId)
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors/{vendorId}")]
+    public ActionResult Update(IFormCollection formContents, int vendorId)
+    {
+      string name = formContents["name"];
+      string contactName = formContents["contactName"];
+      string contactEmail = formContents["contactEmail"];
+      string contactPhone = formContents.ContainsKey("contactPhone") ? formContents["contactPhone"] : "Not given";
+      string contactPronouns = formContents.ContainsKey("contactPronouns") ? formContents["contactPronouns"] : "Not given";
+      Contact newContact = new(contactName, contactEmail, contactPhone, contactPronouns);
+      Vendor updatingVendor = Vendor.Find(vendorId);
+      updatingVendor.Name = name;
+      updatingVendor.Contact = newContact;
+      return RedirectToAction("Show");
+    }
+  }
+}
