1274b0741675903561d2c9146ef12d94320fadec
Hannah Young <hannah@corgibyte.com>
2021-12-17 10:17:32 -0800

    Add tests for Contact; all pass or fail successfully

---
+++ VendorManager.Tests/ModelTests/Contact.Tests.cs
@@ -0,0 +1,53 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorManager.Models;
+
+namespace VendorManager.Tests
+{
+  [TestClass]
+  public class ContactTests
+  {
+
+    [TestMethod]
+    public void ContactConstructor_CreatesInstanceOfContact_Contact()
+    {
+      Contact newContact = new("Test name", "Test email");
+      Assert.AreEqual(typeof(Contact), newContact.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string testName = "Test name";
+      string testEmail = "Test email";
+      Contact newContact = new(testName, testEmail);
+      Assert.AreEqual(testName, newContact.Name);
+    }
+
+    [TestMethod]
+    public void GetPhone_ReturnsPhone_String()
+    {
+      string testName = "Test name";
+      string testEmail = "Test email";
+      Contact newContact = new(testName, testEmail);
+      Assert.AreEqual("Not given", newContact.Email);
+    }
+
+    [TestMethod]
+    public void GetEmail_ReturnsEmail_String()
+    {
+      string testName = "Test name";
+      string testEmail = "Test email";
+      Contact newContact = new(testName, testEmail);
+      Assert.AreEqual(testEmail, newContact.Email);
+    }
+
+    [TestMethod]
+    public void GetPronouns_ReturnsPronouns_String()
+    {
+      string testName = "Test name";
+      string testEmail = "Test email";
+      Contact newContact = new(testName, testEmail);
+      Assert.AreEqual("They", newContact.Email);
+    }
+  }
+}
